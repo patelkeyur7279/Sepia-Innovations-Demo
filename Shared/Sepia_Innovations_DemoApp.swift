3d2616c2fb55777afa18c51a2107a5bb82883694
Keyur Patel <patelkeyur7279@gmail.com>
2022-12-12 13:17:57 +0530

    Initial Commit

---
+++ Shared/Sepia_Innovations_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sepia_Innovations_DemoApp.swift
+//  Shared
+//
+//  Created by Keyur Patel on 12/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Sepia_Innovations_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
